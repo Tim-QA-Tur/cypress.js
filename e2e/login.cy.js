8e8b27c1725d1d35a7d857440ca6397f377ba6bc
Timur <5709037@mail.ru>
2025-08-19 10:52:27 +0700

    Update login.cy.js

--- e2e/login.cy.js
+++ e2e/login.cy.js
@@ -3,8 +3,8 @@ describe('Проверка авторизации', function () {
     it('Верный логин и верный пароль', function () {
          cy.visit('login.qa.studio'); // Зайти на сайт
          cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка слово "забыли пароль" (сенего цвета)
-         cy.get('#mail').type('Cfg.VALID'); // ввел верный логин
-         cy.get('#pass').type('Cfg.VALID'); // ввел верный пароль
+         cy.get('#mail').type('USER_LOGIN.VALID'); // ввел верный логин
+         cy.get('#pass').type('USER_PASSWORD.VALID'); // ввел верный пароль
          cy.get('#loginButton').click(); // нажал войти
          cy.get('#messageHeader').should('be.visible');
          cy.get('#messageHeader').contains('Авторизация прошла успешно'); // проверка текста
@@ -15,7 +15,7 @@ describe('Проверка авторизации', function () {
             cy.visit('login.qa.studio'); // Зайти на сайт
             cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка слово "забыли пароль" (синего цвета)
             cy.get('#forgotEmailButton').click(); // нажать на "Забыли пароль"
-            cy.get('#mailForgot').type('Cfg.VALID'); // ввел валидный адрес почты
+            cy.get('#mailForgot').type('USER_LOGIN.VALID'); // ввел валидный адрес почты
             cy.get('#restoreEmailButton').click(); // нажал "Отправить код"
             cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
             cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // проверка крестика
@@ -25,8 +25,8 @@ describe('Проверка авторизации', function () {
         it('Верный логин и НЕверный пароль', function () {
             cy.visit('https://login.qa.studio'); // Зайти на сайт
             cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка слово "забыли пароль" (синего цвета)
-            cy.get('#mail').type('Cfg.VALID'); // ввел верный логин
-            cy.get('#pass').type('Cfg.INVALID'); // ввел НЕверный пароль
+            cy.get('#mail').type('USER_LOGIN.VALID'); // ввел верный логин
+            cy.get('#pass').type('USER_PASSWORD.INVALID'); // ввел НЕверный пароль
             cy.get('#loginButton').click(); // нажал войти
             cy.get('#messageHeader').should('be.visible');
             cy.get('#messageHeader').contains('Такого логина или пароля нет');
@@ -35,8 +35,8 @@ describe('Проверка авторизации', function () {
         it('НЕверный логин и верный пароль', function () {
             cy.visit('https://login.qa.studio'); // Зайти на сайт
             cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка слово "забыли пароль" (синего цвета)
-            cy.get('#mail').type('Cfg.INVALID'); // ввел НЕверный логин
-            cy.get('#pass').type('Cfg.VALID'); // ввел верный пароль
+            cy.get('#mail').type('USER_LOGIN.INVALID'); // ввел НЕверный логин
+            cy.get('#pass').type('USER_PASSWORD.VALID'); // ввел верный пароль
             cy.get('#loginButton').click(); // нажал войти
             cy.get('#messageHeader').should('be.visible');
             cy.get('#messageHeader').contains('Такого логина или пароля нет');
@@ -45,8 +45,8 @@ describe('Проверка авторизации', function () {
         it('Логин без @', function () {
             cy.visit('https://login.qa.studio'); // Зайти на сайт
             cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка слово "забыли пароль" (синего цвета)
-            cy.get('#mail').type('Cfg.INVALID'); // ввел логин без @
-            cy.get('#pass').type('Cfg.VALID'); // ввел верный пароль
+            cy.get('#mail').type('USER_LOGIN.INVALID'); // ввел логин без @
+            cy.get('#pass').type('USER_PASSWORD.VALID'); // ввел верный пароль
             cy.get('#loginButton').click(); // нажал войти
             cy.get('#messageHeader').should('be.visible');
             cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
@@ -55,8 +55,8 @@ describe('Проверка авторизации', function () {
         it('Проверка строчных букв', function () {
             cy.visit('login.qa.studio'); // Зайти на сайт
             cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка слово "забыли пароль" (сенего цвета)
-            cy.get('#mail').type('Cfg.INVALID'); // ввел логин с строчными буквами
-            cy.get('#pass').type('Cfg.VALID'); // ввел верный пароль
+            cy.get('#mail').type('USER_LOGIN.INVALID'); // ввел логин с строчными буквами
+            cy.get('#pass').type('USER_PASSWORD.VALID'); // ввел верный пароль
             cy.get('#loginButton').click(); // нажал войти
             cy.get('#messageHeader').should('be.visible');
             cy.get('#messageHeader').contains('Авторизация прошла успешно'); // проверка текста
@@ -70,3 +70,4 @@ describe('Проверка авторизации', function () {
  // запуск через теринал: npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome npx cypress open
 
  
+
